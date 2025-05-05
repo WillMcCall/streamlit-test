f88a94332b92eb3eab37b7beaf9eecb30e416990
will-mccall <william@dave-mccall.com>
2025-05-05 12:35:48 -0500

    update

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import pandas as pd
 from datetime import date
 import streamlit as st
-import db
 from utils import clean_jobs, filter_jobs, get_jobs, to_excel
+from github import Github
+import json
+import requests
+import streamlit as st
+
+
+def github_read() -> dict:
+    url = "https://raw.githubusercontent.com/WillMcCall/streamlit_db/main/db.json"
+
+    response = requests.get(url)
+    data = response.json()
+
+    return data
+
+
+def github_write(data: dict):
+    token = st.secrets["GITHUB_TOKEN"]
+    g = Github(token)
+
+    repo = g.get_repo("WillMcCall/streamlit_db")
+    file = "db.json"
+
+    new_content = json.dumps(data, indent=4)
+
+    contents = repo.get_contents(file)
+    repo.update_file(contents.path, "Update db.json", new_content, contents.sha)
 
 
 # Main Logic
 with st.form("my_form"):
-    data = db.github_read()
+    data = github_read()
     
     st.write(data)
     
@@ -37,7 +62,7 @@ if submitted:
         "bais_jobs": bais_jobs,
         "accounting_jobs": accounting_jobs
     }
-    db.github_write(data)
+    github_write(data)
 
     
     time_estimate_seconds = len(locations) * ((len(finance_jobs) + len(bais_jobs) + len(accounting_jobs)) * 7)
